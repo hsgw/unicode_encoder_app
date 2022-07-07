2600dd65a1488ee9dc067b822461a63d24bbcc32
hsgw <urkwtky@gmail.com>
2022-07-07 11:08:23 +0900

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import './App.scss';
+
+import { useState } from 'react';
+
+import UnicodeResult from './UnicodeResult';
+
+function App() {
+  const [inputCharacters, setInputCharacters] = useState<string[]>([]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="title">Unicode Encoder</div>
+        <div className="github">Github</div>
+      </header>
+      <main>
+        <div className="description">
+          <h1>Converts characters into Unicode codepoints.</h1>
+          <p>Type and press Enter to convert.</p>
+        </div>
+        <div className="input">
+          <input
+            type="text"
+            className="textInput"
+            placeholder="Input charactors"
+            onChange={(e) => {
+              setInputCharacters([...e.currentTarget.value]);
+            }}
+          ></input>
+        </div>
+        <div className="result">
+          {inputCharacters[0] ? (
+            inputCharacters.map((c, i) => (
+              <UnicodeResult key={i.toString() + c} char={c} />
+            ))
+          ) : (
+            <div className="noResult">Nothing.</div>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UnicodeResult.tsx
@@ -0,0 +1,30 @@
+import './UnicodeResult.scss';
+
+import React from 'react';
+
+function UnicodeResult({ char }: { char: string }) {
+  const codePoint = char.codePointAt(0) ?? 0;
+  const hexString = ('00000' + codePoint.toString(16).toUpperCase()).slice(-5);
+  const binString = ('000000000000000000' + codePoint.toString(2)).slice(-18);
+
+  return (
+    <div
+      className="unicodeResult"
+      onClick={(e) => {
+        console.log(codePoint);
+      }}
+    >
+      <div className="char">{char}</div>
+      <div className="code">
+        <div className="hex">{`0x${hexString}`}</div>
+        <div className="bin">
+          {binString.split('').map((n, i) => (
+            <span key={char + i.toString()}>{n}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UnicodeResult;
